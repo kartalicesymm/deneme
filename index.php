94305adc6021c32b868f4c8cb5402f809bc6d6b7
Kartalicesymm <kartalicesym@gmail.com>
2022-04-17 19:45:46 +0300

    form olusturuldu

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<html>
+<head>
+    <title>LOGIN</title>
+
+    <style>
+        .form{
+            margin: auto;
+            width: 50%;
+            border: 1 dotted grey;
+        }
+
+    </style>
+</head>
+<body>
+    <div class="form"> 
+        <div>
+        </div>
+         
+    
+    <div>
+</body>
+</html>
+
+
 <?php 
  
  echo "deneme";
