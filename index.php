eaf796d2217cd100516234aba2a72ca7e399bffe
Kartalicesymm <kartalicesym@gmail.com>
2022-04-17 21:22:58 +0300

    form css denemeleri

--- index.php
+++ index.php
@@ -4,10 +4,13 @@
 
     <style>
         .form{
-            margin: auto;
-            width: 50%;
-            height: 50%;
-            border: 3 dotted grey;
+            margin-top: 10%;
+            margin-left:40%;
+            padding: 20px;
+            width: 20%;
+            height: 30%;
+            border: 3px solid red;
+            background-color: yellow;
         }
 
     </style>
