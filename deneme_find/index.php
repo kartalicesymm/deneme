4d152b9a709c7d5aa94c32b6c3f820b1db5d166f
Kartalicesymm <kartalicesym@gmail.com>
2022-06-12 23:08:43 +0300

    index eksik unutulanlar

---
+++ deneme_find/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+<div id="deneme"></div>
+<table>
+  <thead>
+    <td><input name="namem"  type="text"></td>
+    <td> <input type="text"></td>
+  </thead>
+  <tbody>
+    <tr><td>seyma</td><td>kartal</td></tr>
+    <tr><td>arge</td><td>deneme</td></tr>
+  </tbody>
+ <table>
+    
+</body>
+</html>
