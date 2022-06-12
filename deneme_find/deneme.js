7d489c962d27d3168519659f57ad31d4040c401c
Kartalicesymm <kartalicesym@gmail.com>
2022-06-13 01:15:32 +0300

    search in table

--- deneme_find/deneme.js
+++ deneme_find/deneme.js
@@ -1,5 +1,1 @@
-let a=document.querySelector("input[name='namem']");
-function console_yaz(){
-    console.log(a.nodeValue);
-}
-a.addEventListener("keydown",console_yaz  );
+
--- deneme_find/index.php
+++ deneme_find/index.php
@@ -5,21 +5,82 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script scr="deneme.js"></script>
+    
 </head>
 
 <body>
+<button id="deneme">deneme button</button>
 <div id="deneme"></div>
-<table>
+<table id="myTable">
   <thead>
-    <td><input name="namem"  type="text"></td>
-    <td> <input type="text"></td>
+    <td><input id="namem"  type="text"></td>
+    <td> <input id="sunamem" type="text"></td>
   </thead>
-  <tbody>
-    <tr><td>seyma</td><td>kartal</td></tr>
+  <tbody id="myBody">
+    <tr><td >seyma</td><td>kartal</td></tr>
     <tr><td>arge</td><td>deneme</td></tr>
   </tbody>
- <table>
-    
+<table>
+<script>
+//search in namem
+const deneme=document.getElementById("namem")
+deneme.addEventListener("keydown", yaz);
+function yaz(){
+    let td,td_text,filter;
+    //console.log(deneme.value);
+    filter=deneme.value;
+    const table = document.getElementById("myBody");
+    const tr=table.getElementsByTagName("tr");
+    for (let i=0;i<tr.length;i++){
+        td= tr[i].getElementsByTagName('td')[0];
+        if(td){
+            td_text= td.innerText;
+            console.log(td_text);
+            if(td_text.indexOf(filter)>-1){
+                console.log(td_text.indexOf(filter));
+                tr[i].style.display = "";
+            }else{
+                tr[i].style.display = "none";
+            }
+        }
+
+    }
+    //console.log(tr);
+}
+
+
+
+
+//search in surnamem
+const deneme=document.getElementById("sunamem")
+deneme.addEventListener("keydown", yaz);
+function yaz(){
+    let td,td_text,filter;
+    //console.log(deneme.value);
+    filter=deneme.value;
+    const table = document.getElementById("myBody");
+    const tr=table.getElementsByTagName("tr");
+    for (let i=0;i<tr.length;i++){
+        td= tr[i].getElementsByTagName('td')[1];
+        if(td){
+            td_text= td.innerText;
+            console.log(td_text);
+            if(td_text.indexOf(filter)>-1){
+                console.log(td_text.indexOf(filter));
+                tr[i].style.display = "";
+            }else{
+                tr[i].style.display = "none";
+            }
+        }
+
+    }
+    //console.log(tr);
+}
+
+
+
+
+
+    </script>
 </body>
 </html>
